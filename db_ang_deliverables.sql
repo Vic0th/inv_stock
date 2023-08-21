38d69f0e69f4a8b93bbfb388fa9c37376ac43608
Doğukan Soydemirci <dsoydemircii@gmail.com>
2023-08-21 11:01:51 +0300

    ilk calistirma sql ile basarili

--- db_ang_deliverables.sql
+++ db_ang_deliverables.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `city`
 --
-
+USE db_ang_deliverables;
 DROP TABLE IF EXISTS `city`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
