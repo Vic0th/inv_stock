1c9feb0456f41e61b0e64206a06ee977f5ad6eec
Doğukan Soydemirci <dsoydemircii@gmail.com>
2023-08-23 15:52:33 +0300

    basarili calistirma!

--- Ang_IM_Finals/Ang_IM_Finals/Program.cs
+++ Ang_IM_Finals/Ang_IM_Finals/Program.cs
@@ -8,12 +8,11 @@ namespace Ang_IM_Finals
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        
         [STAThread]
         static void Main()
         {
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
